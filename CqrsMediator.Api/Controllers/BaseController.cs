d1de368752f68f112593673559d2adbfffe0ce51
akmal.nodirov@gmail.com <akmal.nodirov@gmail.com>
2023-12-23 20:46:58 +0500

    ci cd yaml added

--- CqrsMediator.Api/Controllers/BaseController.cs
+++ CqrsMediator.Api/Controllers/BaseController.cs
@@ -6,7 +6,7 @@ namespace CqrsMediator.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BaseController(IUnitOfWork unitOfWork, IMapper mapper) : ControllerBase
+public class BaseController(IUnitOfWork unitOfWork, IMapper mapper) : Controller
 {
     protected readonly IUnitOfWork _unitOfWork = unitOfWork;
     protected readonly IMapper _mapper = mapper;
