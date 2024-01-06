73b32f227d10b78631a475d43bfb2013da216718
akmal.nodirov@gmail.com <akmal.nodirov@gmail.com>
2024-01-06 12:42:23 +0500

    some changes

--- CqrsMediator.Api/Controllers/BaseController.cs
+++ CqrsMediator.Api/Controllers/BaseController.cs
@@ -1,13 +1,15 @@
 ﻿using AutoMapper;
 using CqrsMediator.DataService.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace CqrsMediator.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BaseController(IUnitOfWork unitOfWork, IMapper mapper) : Controller
+public class BaseController(IUnitOfWork unitOfWork, IMapper mapper, IStringLocalizer localizer) : Controller
 {
     protected readonly IUnitOfWork _unitOfWork = unitOfWork;
     protected readonly IMapper _mapper = mapper;
+    protected readonly IStringLocalizer _localizer = localizer;
 }
